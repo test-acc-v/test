a5aeed9ba5eed57974c4ce7f4bd09d7a8280d388
Vorobina-Alina <vorobina.a@mail.ru>
2025-02-22 15:46:44 +0300

    third

--- lab3/main.py
+++ lab3/main.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     db.insert("departments", "1,Engineering")
     db.insert("employees_leaves", "1,1,01.06.2025,01.07.2025")
     db.insert("employees_leaves", "2,2,10.09.2025,24.09.2025")
-    db.insert("employees_leaves", "3,2,10.09.2026,24.09.2026")
